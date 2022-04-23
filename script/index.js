a4c546f065cd8b363bebb1df87217c878b911b75
Marija K <kccmrj@outlook.com>
2022-04-23 23:42:32 +0200

    nav functionality, scrollIntoView

--- script/index.js
+++ script/index.js
@@ -2,7 +2,28 @@ const [red, green, blue, opacity] = [170, 247, 235, 0.9];
 const [rojo, verde, azul, opacidad] = [120, 209, 135, 0.1];
 let container = document.querySelector('.container');
 let logo = document.querySelector("#logo");
-let hello = document.querySelector(".hello");
+
+let navHome = document.querySelector("#home");
+let navAbout = document.querySelector("#about");
+let navWork = document.querySelector('#work');
+let navContact = document.querySelector('#contact');
+
+//navigation // scroll
+
+navHome.addEventListener("click", () => {
+  document.querySelector("header").scrollIntoView();
+});
+navAbout.addEventListener('click', () => {
+    document.querySelector(".about").scrollIntoView();
+})
+navWork.addEventListener("click", () => {
+  document.querySelector(".work").scrollIntoView();
+});
+navContact.addEventListener("click", () => {
+  document.querySelector(".contact").scrollIntoView();
+});
+
+//change background on scroll
 
 window.addEventListener('scroll', () => {
 let y = 1 + (window.scrollY || window.pageYOffset) / 150;
