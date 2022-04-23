10408a5a917719bba394c6561ec0873debea3639
Marija K <kccmrj@outlook.com>
2022-04-23 20:33:11 +0200

    background colour change on scroll

---
+++ script/index.js
@@ -0,0 +1,25 @@
+const [red, green, blue, opacity] = [170, 247, 235, 0.9];
+const [rojo, verde, azul, opacidad] = [120, 209, 135, 0.1];
+let container = document.querySelector('.container');
+let logo = document.querySelector("#logo");
+let hello = document.querySelector(".hello");
+
+window.addEventListener('scroll', () => {
+let y = 1 + (window.scrollY || window.pageYOffset) / 150;
+const [r, g, b] = [red, green/y, blue/y].map(Math.round);
+const a = opacity/y/1.1;
+container.style.backgroundColor = `rgba(${r}, ${g}, ${b}, ${a})`;
+})
+
+window.addEventListener("scroll", () => {
+  let y = (window.scrollY || window.pageYOffset) / 300;
+  const [r, g, b] = [rojo, verde/y*1.5, azul].map(Math.round);
+  const a = opacidad *y/1.2;
+  document.body.style.backgroundColor = `rgba(${r}, ${g}, ${b}, ${a})`;
+});
+
+window.addEventListener('scroll', () => {
+   let y = (window.scrollY || window.pageYOffset) / 20;
+   logo.style.opacity = 0.99/y; 
+   hello.style.opacity = 0.99/y;
+})
