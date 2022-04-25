df096f41c4d3c0cf99746d00d2c5621b69bbb0a7
Marija K <kccmrj@outlook.com>
2022-04-25 10:42:07 +0200

    nav link aria-current state changes correctly..
    on scroll thanks to intersection observer API

--- script/index.js
+++ script/index.js
@@ -2,30 +2,48 @@ const [red, green, blue, opacity] = [170, 247, 235, 0.9];
 const [rojo, verde, azul, opacidad] = [120, 209, 135, 0.1];
 let container = document.querySelector('.container');
 let logo = document.querySelector("#logo");
-let hello = document.querySelector('.hello');
-
-let navHome = document.querySelector("#home");
-let navAbout = document.querySelector("#about");
-let navWork = document.querySelector('#work');
-let navContact = document.querySelector('#contact');
+let hello = document.querySelector('section.home');
 let navLinks = document.querySelectorAll('.nav-link');
 let sections = document.querySelectorAll('section');
 
 //navigation // scroll
-
 navLinks.forEach(link => {
     link.addEventListener('click', () => {
-        document.querySelector(`.${link.getAttribute('id')}`).scrollIntoView();
+         document.querySelector(`.${link.getAttribute('id')}`).scrollIntoView({behavior: "smooth"});
         link.setAttribute('aria-current', 'true');
        navLinks.forEach(link0 => {
            if (link0 !== link){
                link0.removeAttribute('aria-current');
-           }
-       })
+             }
+           })
     })
+      
 })
 
-//change background on scroll
+const observer = new IntersectionObserver(entries => {
+ entries.forEach(entry => {
+ if(!entry.isIntersecting) {
+     return
+    }
+    let current = document.querySelector(`#${entry.target.getAttribute("class")}`);
+    current.click()
+    //console.log(entry.target.getAttribute("class"));
+ })
+},
+ {
+  threshold: 0.6
+ }
+)
+
+
+sections.forEach(section => {
+    observer.observe(section)
+})
+
+
+
+
+//change background colour on scroll
 
 window.addEventListener('scroll', () => {
 let y = 1 + (window.scrollY || window.pageYOffset) / 150;
