c765f53e62ba1da927af7972a9e533c326c6d2ea
Marija K <kccmrj@outlook.com>
2022-07-10 19:20:38 +0200

    Create variables.js
    A variables module.

---
+++ script/variables.js
@@ -0,0 +1,17 @@
+export const [red, green, blue, opacity] = [170, 227, 245, 0.9];
+export const [rojo, verde, azul, opacidad] = [120, 209, 135, 0.1];
+
+export let container = document.querySelector(".container");
+export let hello = document.querySelector("section.home");
+export let navLinks = document.querySelectorAll(".nav-link");
+export let about = document.querySelector(".about");
+export let sections = document.querySelectorAll("section");
+
+export let guideX = document.querySelector(".guideX");
+export let guideXOrigin = document.querySelector(".guideX-origin");
+export let guideY = document.querySelector(".guideY");
+export let guideYOrigin = document.querySelector(".guideY-origin");
+
+export let ham = document.querySelector("#ham");
+export let nav = document.querySelector("nav");
+export let close = document.querySelector("#close");
