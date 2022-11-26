a33883847b6c10e0ded5aaed67462e9f6da23ae5
Marija K <kccmrj@outlook.com>
2022-11-26 06:51:14 +0100

    update in attempt to deploy on netlify..

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,5 @@
 const path = require('path');
 const HtmlWebpackPlugin = require("html-webpack-plugin");
-const BundleAnalyzerPlugin =
-  require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
 
 module.exports = {
   mode: "production",
@@ -51,6 +49,5 @@ module.exports = {
       filename: "index.html",
       template: "src/template.html",
     }),
-    new BundleAnalyzerPlugin(),
   ],
 };
